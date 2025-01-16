091a532b9d98d9557d200b2980b60928972a8e2e
Henshaw <josephhensh2000@gmail.com>
2025-01-16 13:29:11 +0100

    Initial commit: add todo and user resources

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TodoModule, UserModule, DatabaseModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
