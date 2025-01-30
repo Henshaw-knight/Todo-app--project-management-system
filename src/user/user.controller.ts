7e38ee54aa6c7979c7a85fa3339dd99f47eb93f4
Henshaw <josephhensh2000@gmail.com>
2025-01-30 13:40:39 +0100

    Update files

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { Request, Response } from 'express'; 
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('signup')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto, @Req() req: Request, @Res() res: Response) {
+    return this.userService.signIn(loginDto, req, res);
+  }
+
+  // @Post('logout')
+  // logout(@Req() req: Request, @Res res: Response) {
+  //   return this.userService.logout(req, res);
+  // }
 
   @Get()
   findAll() {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import {
   HttpException,
   Injectable,
+  Req,
+  Res,
   UnauthorizedException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -9,17 +11,67 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
+import * as argon2 from 'argon2';
+import { LoginDto } from './dto/login.dto';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
     private jwtService: JwtService,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(payload: CreateUserDto) {
+    payload.email = payload.email.toLowerCase();
+    const {email, password, ...rest} = payload;
+    const user = await this.userRepo.findOne({ where: {email: email}});
+    if (user) {
+      throw new HttpException('Email already exists', 400);
+    }
+    const hashedPassword = await argon2.hash(password);
+
+    const userDetails = await this.userRepo.save({
+      ...rest, email, password: hashedPassword
+    });
+
+    delete userDetails.password;
+    const userPayload = {email: userDetails.email, id: userDetails.id};
+    return {
+      access_token: await this.jwtService.signAsync(userPayload),
+    };
   }
+
+  async signIn(payload: LoginDto, @Req() req: Request, @Res() res: Response) {
+    const { email, password} = payload;
+
+    const user = await this.userRepo.findOneBy({ email });
+
+    if (!user) {
+      throw new HttpException('No email found', 400)
+    }
+
+    const checkedPassword = await this.verifyPassword(user.password, password);
+    if (!checkedPassword) {
+      throw new HttpException('Sorry password does not exist', 400);
+    }
+    const token = await this.jwtService.signAsync({
+      email: user.email,
+      id: user.id
+    });
+
+    res.cookie('isAuthenticated', token, {
+      httpOnly: true,
+      maxAge: 1 * 60 * 60 * 1000
+    });
+
+    return res.send({
+      success: true,
+      userToken: token
+    })
+  }
+
+  // async logout(@Req)
 
   findAll() {
     return `This action returns all user`;
@@ -37,8 +89,17 @@ export class UserService {
     return `This action removes a #${id} user`;
   }
 
+  async verifyPassword(hashedPassword: string, plainPassword: string,): Promise<boolean> {
+    try{
+      return await argon2.verify(hashedPassword, plainPassword);
+    } catch (err) {
+      console.log(err.message);
+      return false;
+    }
+  }
+
   async findEmail(email) {
-    const userEmail = await this.userRepository.findOneBy({ email });
+    const userEmail = await this.userRepo.findOneBy({ email });
 
     if (!userEmail) {
       throw new HttpException('Email already exists', 400);
@@ -51,7 +112,7 @@ export class UserService {
     // It tries to extract the authorization header from the incoming request headers.
     // This header typically contains the token used for authentication.
     if (authorizationHeader) {
-      const token = authorizationHeader.replace('Bearer', '');
+      const token = authorizationHeader.replace('Bearer ', '');
       const secret = process.env.JWTSECRET;
       // checks if the authorization header exists. If not, it will skip to the else block
       // and throw an error.
@@ -61,7 +122,7 @@ export class UserService {
         let id = decoded['id'];
         // After verifying the token, the function extracts the user's id from the decoded token payload.
 
-        let user = await this.userRepository.findOneBy({ id });
+        let user = await this.userRepo.findOneBy({ id });
 
         return { id: id, name: user.name, email: user.email, role: user.role };
       } catch (error) {
